02ffdab4773d56a284cbc6db420afb86850ca861
Aman Singh Samant <56504833+Aman-Singh-Samant@users.noreply.github.com>
2023-11-20 14:15:29 +0530

    updated the header made the profile page private      (if user is null means no user profile page can not be accessed it will lead to sign-in page).

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,25 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import Home from './pages/Home'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
